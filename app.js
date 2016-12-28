b6611c6a49ebd9f5acb37c14dce3ce50c2737991
Zach Lawryk <zach.lawryk@gmail.com>
2016-12-28 10:03:15 -0800

    updated app.js with jquery and contents of input

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var main = function () {
 	"use strict" ;
 
-$(".comment-input button").on("click" , function (event){
-	console.log("Hello Worldy!");
+	$(".comment-input button").on("click" , function (event){
+		var $new_comment = $("<p>"),
+			comment_text = $(".comment-input input").val();
+	
+		$new_comment.text(comment_text);
+
+		$(".comments").append($new_comment);
 });
+
 };
 
 $(document).ready(main);
