83204f4e36054647d5f3594da468e28aeb5c240b
Zach Lawryk <zach.lawryk@gmail.com>
2016-12-28 10:48:19 -0800

    added fadin, new event listener and at page 109

--- app.js
+++ app.js
@@ -2,13 +2,32 @@ var main = function () {
 	"use strict" ;
 
 	$(".comment-input button").on("click" , function (event){
-		var $new_comment = $("<p>"),
-			comment_text = $(".comment-input input").val();
-	
-		$new_comment.text(comment_text);
+		var $new_comment; 
 
-		$(".comments").append($new_comment);
+		if($(".comment-input input").val() !=="") {
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
+			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
+			$("comment-input input").val("");
+
+		}
+
 });
+
+	$(".comment-input input").on("keypress", function(event) {
+		var $new_comment;
+
+		if (event.keyCode == 13) {
+			if ($(".comment-input input").val() !== "") {
+				$new_comment = $("<p>").text($(".comment-input input").val());
+				$new_comment.hide();
+				$(".comments").append($new_comment);
+				$new_comment.fadeIn();
+				$("comment-input input").val("");
+			}
+		}
+	});
 
 };
 
